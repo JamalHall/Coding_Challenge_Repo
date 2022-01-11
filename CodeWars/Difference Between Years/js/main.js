f03da3226f0de341bdbb74af61b8c7fd3b373867
Jamal Hall <jamalhallwebdev@gmail.com>
2022-01-11 10:41:07 -0500

    first try

---
+++ CodeWars/Difference Between Years/js/main.js
@@ -0,0 +1,20 @@
+/* Write a function that receives two strings as parameter. This strings are in the following format of date: YYYY/MM/DD. Your job is: Take the years and calculate the difference between them.
+
+Examples:
+'1997/10/10' and '2015/10/10' -> 2015 - 1997 = returns 18 
+'2015/10/10' and '1997/10/10' -> 2015 - 1997 = returns 18
+At this level, you don't need validate months and days to calculate the difference.
+
+*/
+
+var howManyYears = function(date1, date2){
+  date1=Number(date1.split('/').filter((e,i)=> i==0))*-1
+  date2=Number(date2.split('/').filter((e,i)=> i==0))
+  return Math.abs(date1 + date2)
+}
+
+console.log(howManyYears('1997/10/10', '2015/10/10'), 18);
+console.log(howManyYears('1990/10/10', '2015/10/10'), 25);
+console.log(howManyYears('2015/10/10', '1990/10/10'), 25);
+console.log(howManyYears('1992/10/24', '2015/10/24'), 23);
+console.log(howManyYears('2018/10/10', '2000/10/10'), 18);
