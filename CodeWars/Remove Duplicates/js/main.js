7a05bbdb7db07c5cb0c8be8c6af77c54af07ff39
JHall <JamalHallWebDev@gmail.com>
2023-10-06 19:55:52 -0400

    added logs

--- CodeWars/Remove Duplicates/js/main.js
+++ CodeWars/Remove Duplicates/js/main.js
@@ -1,14 +1,23 @@
 /*
-Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
 
-domainName("http://github.com/carbonfive/raygun") == "github" 
-domainName("http://www.zombie-bites.com") == "zombie-bites"
-domainName("https://www.cnet.com") == "cnet"
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118/train/javascript
 */
 function distinct(a) {
-  let set = new Set()
-   for(e in a){
-      set.add(a)
+   let set = new Set()
+   for (e of a) {
+      set.add(e)
    }
    return Array.from(set)
 }
+console.log(distinct([1]), [1]);
+console.log(distinct([1,2]), [1,2]);
+console.log(distinct([1,1,2]), [1,2]);
