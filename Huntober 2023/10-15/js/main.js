c802df7a01515c7df3732d200c25c9a34f8cbb81
JHall <JamalHallWebDev@gmail.com>
2023-10-16 08:57:47 -0400

    using recursion

--- Huntober 2023/10-15/js/main.js
+++ Huntober 2023/10-15/js/main.js
@@ -13,6 +13,24 @@ function twoDecimalPlaces(n) {
   return roundedNumber;
 }
 
+//long way using recursion
+
+function twoDecimalPlaces(n) {
+  function checkLast(n2){
+  let num = Math.trunc(n2*1000)
+  let num2 = num.toString().split('')
+    let num3 = num2[num2.length - 1]
+    console.log(n, num, num2, num3, +num3)
+  return +num3
+  }  
+  let num4 = checkLast(n)>=5?+Math.trunc(n*100)+1:Math.trunc(n*100)
+  console.log(checkLast(num4), num4)
+  if(checkLast(num4/100)>=5){
+    return twoDecimalPlaces(num4/100)
+  }   
+  return num4/100
+  }
+
 console.log(twoDecimalPlaces(4.659725356), 4.66, "didn't work for 4.659725356");
 console.log(twoDecimalPlaces(173735326.3783732637948948), 173735326.38, "didn't work for 173735326.3783732637948948");
 console.log(twoDecimalPlaces(4.653725356), 4.65, "didn't work for 4.653725356");
