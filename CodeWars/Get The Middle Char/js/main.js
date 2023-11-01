7450c844bb319e611a2f6a7def285a032bcb048c
JHall <JamalHallWebDev@gmail.com>
2023-11-01 11:27:32 -0400

    8kata simple practice

--- CodeWars/Get The Middle Char/js/main.js
+++ CodeWars/Get The Middle Char/js/main.js
@@ -1,31 +1,16 @@
-// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+// Alan is known for referring to the temperature of the apple turnover as Hotter than the sun!. According to space.com the temperature of the sun's corona is 2,000,000 degrees Celsius, but we will ignore the science for now.
 
-// #Examples:
+// Task
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
 
-// Kata.getMiddle("test") should return "es"
+// Note: Input will either be a positive integer (or a string for untyped languages).
+// https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/javascript
 
-// Kata.getMiddle("testing") should return "t"
-
-// Kata.getMiddle("middle") should return "dd"
-
-// Kata.getMiddle("A") should return "A"
-// #Input
-
-// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
-
-// #Output
-
-// The middle character(s) of the word represented as a string.
-//https://www.codewars.com/kata/56747fd5cb988479af000028/train/javascript
-
-function getMiddle(s)
-{
-    mid = s.length%2!==0?s.length/2:(s.length-1)/2
-    string = s.substring(mid, s.length % 2 !== 0 ? mid + 1 : mid + 2)
-    return string
+function apple(x){
+    let moreThan = 'It\'s hotter than the sun!!'
+    let lessThan = 'Help yourself to a honeycomb Yorkie for the glovebox.'
+    return x**2>10000?moreThan:lessThan
 }
 
-console.log(getMiddle("test"),"es");
-console.log(getMiddle("testing"),"t");
-console.log(getMiddle("middle"),"dd");
-console.log(getMiddle("A"),"A");
+console.log(apple('50'), 'It\'s hotter than the sun!!'); 
+console.log(apple(4), 'Help yourself to a honeycomb Yorkie for the glovebox.'); 
