6923d22ea122dbf8f976df8091ddc422ae6dfecf
JHall <JamalHallWebDev@gmail.com>
2023-11-16 18:14:39 -0500

    Update main.js

--- CodeWars/String incrementer/js/main.js
+++ CodeWars/String incrementer/js/main.js
@@ -17,42 +17,42 @@ foo099 -> foo100
 
 https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/javascript
 */
-function incrementString(string) {
-  let arrayString = string.split('')
-  let concatString = string.split()
-  let number = 0
-  const regex = new RegExp("[0-9]",i)
-  for (var i = arrayString.length - 1; i >= 0; --i) {   
-    if (regex.test(+arrayString[i]) && regex.test(+arrayString[i-1])) {
-      number +=arrayString[i]    
-    } else if (regex.test(+arrayString[i])) {     
-      number += arrayString[i]
-   }
-  }
-  const numberLength = (+number).toString().length
-  const sliceString = string.slice(0,-numberLength)
-  console.log(string,sliceString,number, +number+1) 
- return sliceString+(+number+1)
-}
+// function incrementString(string) {
+//   let arrayString = string.split('')
+//   let concatString = string.split()
+//   let number = 0
+//   const regex = new RegExp("[0-9]",i)
+//   for (var i = arrayString.length - 1; i >= 0; --i) {   
+//     if (regex.test(+arrayString[i]) && regex.test(+arrayString[i-1])) {
+//       number +=arrayString[i]    
+//     } else if (regex.test(+arrayString[i])) {     
+//       number += arrayString[i]
+//    }
+//   }
+//   const numberLength = (+number).toString().length
+//   const sliceString = string.slice(0,-numberLength)
+//   console.log(string,sliceString,number, +number+1) 
+//  return sliceString+(+number+1)
+// }
 
-let incrementString = str => str.replace(/([0-8]|\d?9+)?$/, (e) => e ? + e + 1 : 1)
+//let incrementString = str => str.replace(/([0-8]|\d?9+)?$/, (e) => e ? + e + 1 : 1)
 
-function incrementString(str){
-	var c = str[str.length-1];
-		switch(c){
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8': return str.substring(0, str.length-1) + (parseInt(c)+1);
-            case '9': return incrementString(str.substring(0, str.length-1)) + 0;
-            default: return str+"1";
-                }
-}
+// function incrementString(str){
+// 	var c = str[str.length-1];
+// 		switch(c){
+//             case '0':
+//             case '1':
+//             case '2':
+//             case '3':
+//             case '4':
+//             case '5':
+//             case '6':
+//             case '7':
+//             case '8': return str.substring(0, str.length-1) + (parseInt(c)+1);
+//             case '9': return incrementString(str.substring(0, str.length-1)) + 0;
+//             default: return str+"1";
+//                 }
+// }
 
 
 const incrementString = strng =>
